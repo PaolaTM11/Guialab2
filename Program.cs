d3fa22313e24dafbc5411baf7e5ec9d591c96f27
Paola TM <purplehoppe04@gmail.com>
2026-09-23 19:04:45 -0500

    Requerimiento 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("=================================");
+Console.WriteLine("   SOPORTE ACADÉMICO");
+Console.WriteLine("=================================");
+
+string codigo;
+string nombre;
+string tipoConsulta;
+string descripcion;
+
+Console.WriteLine("Ingrese el código del estudiante:");
+codigo = Console.ReadLine();
+
+Console.WriteLine("Ingrese el nombre del estudiante:");
+nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese el tipo de consulta:");
+tipoConsulta = Console.ReadLine();
+
+Console.WriteLine("Ingrese una descripción:");
+descripcion = Console.ReadLine();
+
+Console.WriteLine("\nSOLICITUD REGISTRADA");
+Console.WriteLine("Código: " + codigo);
+Console.WriteLine("Nombre: " + nombre);
+Console.WriteLine("Tipo de consulta: " + tipoConsulta);
+Console.WriteLine("Descripción: " + descripcion);

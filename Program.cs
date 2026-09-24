a6995e43a304ee14a2c9751462fba9292ffb135b
Paola TM <purplehoppe04@gmail.com>
2026-09-23 19:59:14 -0500

    Req 5

--- Program.cs
+++ Program.cs
@@ -1,57 +1,105 @@
-﻿Console.WriteLine("=================================");
-Console.WriteLine("   SOPORTE ACADÉMICO");
-Console.WriteLine("=================================");
-
-string codigo;
-string nombre;
-string tipoConsulta;
-string descripcion;
-
-Console.WriteLine("Ingrese el código del estudiante:");
-codigo = Console.ReadLine();
-while (codigo == "" || codigo.Length < 5)
+﻿class PRogram 
 {
-    Console.WriteLine("Código inválido.");
-    Console.WriteLine("Debe tener al menos 5 caracteres.");
-    Console.WriteLine("Ingrese nuevamente el código:");
+    static void Main()
+    {
+     MostrarMenu();
+     Console.WriteLine("=================================");
+     Console.WriteLine("   SOPORTE ACADÉMICO");
+     Console.WriteLine("=================================");
 
-    codigo = Console.ReadLine();
-}
+     string codigo;
+     string nombre;
+     string tipoConsulta;
+     string descripcion;
 
-Console.WriteLine("Ingrese el nombre del estudiante:");
-nombre = Console.ReadLine();
+     Console.WriteLine("Ingrese el código del estudiante:");
+     codigo = Console.ReadLine();
+     while (codigo == "" || codigo.Length < 5)
 
-Console.WriteLine("Ingrese el tipo de consulta:");
-tipoConsulta = Console.ReadLine();
-string[] tipos = { "matricula", "pagos", "constancia", "plataforma", "otro" };
+    {
+       Console.WriteLine("Código inválido.");
+       Console.WriteLine("Debe tener al menos 5 caracteres.");
+      Console.WriteLine("Ingrese nuevamente el código:");
 
-bool valido = false;
+       codigo = Console.ReadLine();
+    }
 
-   while (valido == false)
-  {
-     for (int i = 0; i < tipos.Length; i++)
-     {
+   Console.WriteLine("Ingrese el nombre del estudiante:");
+   nombre = Console.ReadLine();
+
+   Console.WriteLine("Ingrese el tipo de consulta:");
+   tipoConsulta = Console.ReadLine();
+   string[] tipos = { "matricula", "pagos", "constancia", "plataforma", "otro" };
+
+    bool valido = false;
+
+    while (valido == false)
+    {
+      for (int i = 0; i < tipos.Length; i++)
+      {
         if (tipoConsulta == tipos[i])
         {
             valido = true;
         }
-     }
+      }
 
-     if (valido == false)
-     {
+      if (valido == false)
+      {
         Console.WriteLine("Tipo de consulta incorrecto.");
         Console.WriteLine("Las opciones son: matricula, pagos, constancia, plataforma u otro.");
         Console.WriteLine("Ingrese nuevamente el tipo de consulta:");
 
         tipoConsulta = Console.ReadLine();
+      }
+    }
+
+    Console.WriteLine("Ingrese una descripción:");
+    descripcion = Console.ReadLine();
+    string prioridad = AsignarPrioridad(tipoConsulta);
+
+    Console.WriteLine("\nSOLICITUD REGISTRADA");
+     
+    Console.WriteLine("Código: " + codigo);
+    Console.WriteLine("Nombre: " + nombre);
+    Console.WriteLine("Tipo de consulta: " + tipoConsulta);
+    Console.WriteLine("Descripción: " + descripcion);
+    Console.WriteLine("Prioridad:"+prioridad);
+    }
+
+
+  static void MostrarMenu()
+    {
+        Console.WriteLine("=================================");
+        Console.WriteLine("       SOPORTE ACADÉMICO");
+        Console.WriteLine("=================================");
+        Console.WriteLine("1. Registrar solicitud");
+        Console.WriteLine("2. Mostrar solicitudes");
+        Console.WriteLine("3. Salir");
+     }    
+
+   static string AsignarPrioridad(string tipoConsulta)
+   {
+     if (tipoConsulta == "matricula")
+     {
+        return "Alta";
      }
-   }
+     else if (tipoConsulta == "pagos")
+     {
+        return "Alta";
+    }
+     else if (tipoConsulta == "plataforma")
+     {
+        return "Alta";
+     }
+     else if (tipoConsulta == "constancia")
+     {
+        return "Media";
+     }
+     else
+     {
+        return "Baja";
+     }
+    }
 
-Console.WriteLine("Ingrese una descripción:");
-descripcion = Console.ReadLine();
 
-Console.WriteLine("\nSOLICITUD REGISTRADA");
-Console.WriteLine("Código: " + codigo);
-Console.WriteLine("Nombre: " + nombre);
-Console.WriteLine("Tipo de consulta: " + tipoConsulta);
-Console.WriteLine("Descripción: " + descripcion);
+}
